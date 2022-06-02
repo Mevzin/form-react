80108e348e5903b9d9b35bcf655a091d3a1823ee
Thiago Torres <thiagomev@gmail.com>
2022-06-01 21:22:01 -0300

    Remove fiveicon

--- webpack.prod.js
+++ webpack.prod.js
@@ -47,9 +47,6 @@ module.exports = merge( common, {
     }),
     new MiniCssExtractPlugin({
       filename: 'main-bundle-[hash].css'
-    }),
-    new FaviconsWebpackPlugin({
-      logo: './public/favicon.jpeg'
     })
   ]
 })
