7a3ac6f0b911e2166e09ffb5bc3f25e91b30a4a5
Thiago Torres <thiagomev@gmail.com>
2022-06-14 21:22:16 -0300

    Refresh webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,52 +1,64 @@
 // const { DefinePlugin } = require('webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
-const common = require('./webpack.common');
-const { merge } = require('webpack-merge');
+const common = require("./webpack.common");
+const { merge } = require("webpack-merge");
 
-module.exports = merge( common, {
-  mode: 'production',
+module.exports = merge(common, {
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js(x?)$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
-      }, {
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.(scss|sass)$/,
         use: [
           {
-            loader: MiniCssExtractPlugin.loader
-          }, {
-            loader: 'css-loader',
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: "css-loader",
             options: {
-              modules: true
-            }
-          }, {
-            loader: 'sass-loader'
-          }
-        ]
-      }, {
+              modules: true,
+            },
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
-  externals: {
-    react: 'React',
-    axios: 'axios',
-    'react-dom': 'ReactDOM',
-    'react-router-dom': 'ReactRouterDOM', 
-  },
+  // externals: {
+  //   react: 'React',
+  //   axios: 'axios',
+  //   'react-dom': 'ReactDOM',
+  //   'react-router-dom': 'ReactRouterDOM',
+  // },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './template.prod.html'
+      template: "./template.prod.html",
     }),
     new MiniCssExtractPlugin({
-      filename: 'main-bundle-[hash].css'
-    })
-  ]
-})
+      filename: "main-bundle-[hash].css",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./src/assets/favicon.png",
+      outputPath: "img",
+    }),
+  ],
+});
